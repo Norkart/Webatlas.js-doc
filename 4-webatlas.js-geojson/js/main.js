547a840cb8f1a83c43e38a511a3749746d65f72b
Robert Nordan <robert.nordan@norkart.no>
2014-02-26 09:11:56 +0100

    Add customer IDs to examples
    Allows for better logging of where the materials are used.

--- 4-webatlas.js-geojson/js/main.js
+++ 4-webatlas.js-geojson/js/main.js
@@ -3,7 +3,7 @@ var map;
 
 $(document).ready(function() {
     //starter kartmotoren og putter det i div med id="map"
-    map = new WebatlasMap('map');
+    map = new WebatlasMap('map', {customer: 'WA_JS_V3_Coursework'});
 
     //endrer senterpunkt til koordinatene og setter zoomnivå til 5
     map.setView(new L.LatLng(64.0107043, 11.4901134), 5);
