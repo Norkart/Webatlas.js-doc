6b842841934f870a683fbe4b2f62c0b816740faf
Alexander <alexanno@gmail.com>
2014-04-24 18:11:47 +0200

    fix coloring and added heatmap, coverage, clustering

--- 8-omsetningsdata/js/main.js
+++ 8-omsetningsdata/js/main.js
@@ -37,19 +37,29 @@ $(document).ready(function() {
         fillOpacity: 0.8
     };
 
+    //liste som skal holde punktdata til bruk i heatmap og dekningskart
+    var eiendomspunkter = [];
+    var eiendomspunkterMedOmsetning = [];
+
     //Start "geoJson"-motoren til Leaflet. Den tar inn et JSON-objekt i en variabel. Denne har vi definert i JSON-filen i index.html
     var eiendommer = L.geoJson(eiendompktGeoJSON, {
         onEachFeature: visPopup,//vi refererer til funksjonen vi skal kalle. Husk at funksjonen også er et objekt
         pointToLayer: function(feature, latlng) {
-            //console.log(feature.properties);
-            //console.log(feature.properties.OBJTYPE);
-
-            if(feature.properties.omsetningsbelop <= 100000) {
+            //legg til et punkt i punktlisten. Punktet er en liste med "lat, lng"
+            eiendomspunkter.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]]);
+            eiendomspunkterMedOmsetning.push([
+                feature.geometry.coordinates[1], 
+                feature.geometry.coordinates[0], 
+                feature.properties.omsetningsbelop
+            ]);
+            //endre stilen for dette objektet
+            if(feature.properties.omsetningsbelop <= 1000000) {
                 geojsonMarkerOptions.fillColor = "#00ff00";
-            } else if (feature.properties.omsetningsbelop <= 500000) {
+            } else if (feature.properties.omsetningsbelop > 1000000 && feature.properties.omsetningsbelop <= 5000000) {
                 geojsonMarkerOptions.fillColor = "#ff0000";
             } else {
-                //default, don't change the styling
+                //default
+                geojsonMarkerOptions.fillColor = "#0000ff";
             }
             return L.circleMarker(latlng, geojsonMarkerOptions);
         }
@@ -58,34 +68,65 @@ $(document).ready(function() {
     //legg til eiendomspunktene til "layer control"
     map.LayerControl.addOverlay(eiendommer, "Eiendommer (p)");
 
-    //Sett opp stil til de nye sirkelmarkørene
-    var flateStyle = {
-        fillColor: "#0000ff",
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.5
-    };
-
     //Start "geoJson"-motoren til Leaflet. Den tar inn et JSON-objekt i en variabel. Denne har vi definert i JSON-filen i index.html
     var eiendommerPolygon = L.geoJson(eiendompolygonGeoJSON, {
         onEachFeature: function(feature, latlng) {
             visPopup(feature,latlng);
-            
-            flateStyle.fillColor = "#0000ff";//reset
-            
-            if(feature.properties.omsetningsbelop <= 100000) {
+        },
+        style: function(feature) {
+            var flateStyle = {
+                fillColor: "#0000ff",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.5
+            };
+            if(feature.properties.omsetningsbelop <= 1000000) {
                 flateStyle.fillColor = "#00ff00";
-            } else if (feature.properties.omsetningsbelop <= 500000) {
+            } else if (feature.properties.omsetningsbelop > 1000000 && feature.properties.omsetningsbelop <= 5000000) {                
                 flateStyle.fillColor = "#ff0000";
             } else {
-                //default, don't change the styling
+                flateStyle.fillColor = "#0000ff";
             }
-        },
-        style: flateStyle
+            return flateStyle;            
+        }
     }).addTo(map);
 
     //legg til eiendomspunktene til "layer control"
     map.LayerControl.addOverlay(eiendommerPolygon, "Eiendommer (f)");    
+
+/** Mer avanserte visualiseringer av datasettene */
+
+    //start opp heatmap-motoren - vi bruker punktlisten vi lagde ovenfor og setter parametere
+    var heatmapLayer = L.heatLayer(eiendomspunkterMedOmsetning, {
+        radius: 60,
+        max: 0.8
+    });
+
+    //Legg til heatmap til layer control
+    map.LayerControl.addOverlay(heatmapLayer, "Heatmap");
+
+    //start opp maskeringsmotoren og sett nødvendige parametere
+    var coverageLayer = new L.TileLayer.MaskCanvas({
+        'opacity': 0.8,
+        radius: 500,
+        useAbsoluteRadius: true,
+        'attribution': ''
+    });
+
+    //knytt punktlisten vår til maskeringsmotoren
+    coverageLayer.setData(eiendomspunkter);
+    //legg til maskering som eget lag i layer control
+    map.LayerControl.addOverlay(coverageLayer, "Dekning");
+
+    //start clustermotoren
+    var clusterMarkers = new L.MarkerClusterGroup();
+
+    //legg til eiendommer-laget til clustermotoren og legg til kartet
+    clusterMarkers.addLayer(eiendommer);
+    //legg også til som eget lag i layer control
+    map.LayerControl.addOverlay(clusterMarkers, "Eiendommer (cluster)");    
+
+
 
 });
